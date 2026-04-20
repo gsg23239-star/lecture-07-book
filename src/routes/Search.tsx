5c807be9f7e7ace2b1099b0eb8abab366feac129
LINE <gsg2323@naver.com>
2026-04-20 09:36:51 +0900

    본인 작업

---
+++ src/routes/Search.tsx
@@ -0,0 +1,31 @@
+import {useSearchParams} from "react-router";
+import { useEffect, useState} from "react";
+
+type BookItem = {
+    id: string;
+    volumeInfo: {
+        title: string;
+        author?: string;
+        description?: string;
+        imageLink?: {
+            thumbnail?: string;
+            small?: string;
+        }
+    }
+}
+function Search() {
+    // query string으로 들어온 값을 꺼내오기 위해서는 useSearchParams를 사용
+
+    // useParams를 사용할 때는 const { id } = useParams();
+    // useSearchParams는 useState와 사용법이 동일
+    const [params, setParams] = useSearchParams();  // queryString 내용이 params에 담겨 나옴
+    const k = params.get("keyword");  // "수학" 이라는게 있을수도 있지만, 없을수도 있음
+
+    // keyword 준비 됐으니, API를 통해 요청한 정보를 받아다가 화면에 출력만 해주면 됨
+    const [loading, setloadig] = useState(true);
+    const [list, setList] = useState([]);
+
+    return <></>;
+}
+
+export default Search;
